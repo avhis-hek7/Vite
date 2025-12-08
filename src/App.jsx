4745edfbb71916b92d199e8c28b62343b1b8ac6e
avhis-hek7 <sharmaavishek679@gmail.com>
2025-12-08 20:45:43 +0545

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
+import { useState } from "react"
 
-import Section1 from './Components/Section1/Section1'
-import Section2 from './Components/Section2/Section2'
+
 
 const App = () => {
+const [a, seta] = useState(20);
+const [username, setusername] = useState('Avhishek')
+const [users, setusers] = useState([10,20,30,40])
 
 
-  const users = [
-    {
-      img:'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D',
-      intro:'',
-      tag:'Satisfied'
-    },
-    {
-      img:'https://images.unsplash.com/photo-1522206038088-8698bcefa6a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHdvcmtpbmclMjBwcm9mZXNzaW9uYWx8ZW58MHx8MHx8fDA%3D',
-      intro:'',
-      tag:'Underserved'
-    },
-    {
-      img:'https://plus.unsplash.com/premium_photo-1661641353075-f0eaf2d82aae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D',
-      intro:'',
-      tag:'Underbanked'
-    }
-  ]
+
+function changeNum(){
+    seta(30)
+    setusername('Arjun')
+    setusers([50,60,70,80])
+}
+    
+
+
   return (
     <div>
-    <Section1 users={users}/>
-    <Section2/>
+      <h1>Value of a is {a} <br /> {users} <br />Value of user name is {username}</h1>
+      <button onClick={changeNum}>Click</button>
+
     </div>
   )
+
 }
 
 export default App
-
-
