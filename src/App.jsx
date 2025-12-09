353d2e94ba0acf29cdae009d01c39b840cdb5355
avhis-hek7 <sharmaavishek679@gmail.com>
2025-12-09 22:00:01 +0545

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
+  
+// const  user = {
+//   username:'Avhishek',
+//   age:15,
+//   city:'karkineta'
 
-  const [title, setTitle] = useState('')
-  const [details, setdDetails] = useState('')
-
-  const [task, setTask] = useState([])
-
-
-  const submitHandle = (e) => {
-    e.preventDefault()
-
-    const copyTask = [...task];
-    copyTask.push({ title, details })
-    setTask(copyTask)
+// }
+// localStorage.setItem('user',JSON.stringify(user);
 
 
-    setTitle('')
-    setdDetails('')
-  }
-
-  const deleteNote = (idx) => {
-    const copyTask = [...task];
-    copyTask.splice(idx, 1)
-    setTask(copyTask)
+// const user = JSON.parse(localStorage.getItem('user'))
+// console.log(use)
 
 
-  }
 
 
   return (
-    <div className='h-screen bg-black text-white'>
-      <form onSubmit={(e) => {
-        submitHandle(e)
-      }} className='flex items-start p-10 flex-col gap-5'>
-
-        {/*First input for Headings*/}
-        <input type="text"
-          placeholder='Enter Notes Heading'
-          className='w-1/2 px-5 py-2 border-2 rounded '
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value)
-
-          }}
-
-        />
-
-        {/*Details Input*/}
-        <textarea type="text"
-          placeholder='Write details'
-          className='w-1/2 px-5 py-2 h-20 border-2 rounded '
-          value={details}
-          onChange={(e) => {
-            setdDetails(e.target.value)
-          }}
-
-        />
-        <button className='bg-white text-black px-5 py-2 rounded'> Add Notes</button>
-
-      </form>
-      <div className='flex flex-wrap gap-10 p-10'>
-        <h1>Your Notes</h1>
-        {task.map(function (elem, idx) {
-          return <div key={idx} className='h-52 w-40 rounded-xl p-4 text-black bg-white'>
-            <div>
-              <h3 className='leading-tight text-xl font-bold'>{elem.title}</h3>
-              <p>{elem.details}</p>
-            </div>
-            <button onClick={() => {
-              deleteNote(idx)
-            }} className='w-full bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
-          </div>
-
-        })}
-      </div>
+    <div>
+      APP
     </div>
   )
 }
