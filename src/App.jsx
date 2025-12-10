479e8533108c77230321aff49e71eb625ab94c92
avhis-hek7 <sharmaavishek679@gmail.com>
2025-12-10 08:57:57 +0545

    another example of use effects

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,48 @@ import React, { useEffect, useState } from 'react'
 
 
 const App = () => {
-  const [num, setNum] = useState(0)
-  const [num2, setNum2] = useState(100)
-  useEffect(function(){
-    console.log('use effect is running');
+  // const [num, setNum] = useState(0)
+  // const [num2, setNum2] = useState(100)
+  // useEffect(function(){
+  //   console.log('use effect is running');
 
-  },[num])
+  // },[num])
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+
+  function aChanging(){
+    console.log('A value changed');
+    
+
+  }
+
+   function bChanging(){
+    console.log('B value changed');
+  }
+
+  useEffect (function(){
+   
+    console.log('use Effect is running');
+    
+  },[a])
   return (
     <div>
-      <h1> Value of num1 is{num}</h1>
+
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button
+      onClick={()=>{
+        setA(a+1)
+  
+      }}
+      >Change A</button>
+      <button
+         onClick={()=>{
+        setB(b-1)
+  
+      }}
+      >Change B</button>
+      {/* <h1> Value of num1 is{num}</h1>
       <h1> Value of num2 is{num2}</h1>
       <button 
       // onClick={()=>{
@@ -32,7 +65,7 @@ const App = () => {
        }}
 
 
-      >Click</button>
+      >Click</button> */}
     </div>
   )
 }
