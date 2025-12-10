b4d93bc7263a51ceb05d2232b4aaeac420a0e7f7
avhis-hek7 <sharmaavishek679@gmail.com>
2025-12-10 08:43:13 +0545

    Useeffects

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,41 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
 
 const App = () => {
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(100)
+  useEffect(function(){
+    console.log('use effect is running');
 
-  const [data, setData] = useState([])
-
-
-  // async  function getData(){
-  //   const response =  await fetch('https://jsonplaceholder.typicode.com/todos')
-
- const getData =   async () =>{
-  // const response = await fetch('https://jsonplaceholder.typicode.com/users')
-  // const data =  await response.json()
-  // console.log(data)
-
-  // const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-  // console.log(response.data);
-  
-
-  const response = await axios.get('https://picsum.photos/v2/list')
-  // console.log(response.data);  
-    
-  setData(response.data)
-  }
-  
+  },[num])
   return (
     <div>
-      <button onClick={getData}>Get Data</button>
-      <div>
-        {data.map(function(elem,idx){
-          return <h3>{elem.author}, {idx}</h3>
-        })}
-      </div>
+      <h1> Value of num1 is{num}</h1>
+      <h1> Value of num2 is{num2}</h1>
+      <button 
+      // onClick={()=>{
+      //   setNum(num+1)
+        
+
+      // }}
+      // onDoubleClick={()=>{
+      //   setNum2(num2+10)
+      // }}
+        
+
+      onMouseEnter={()=>{
+        setNum(num+1)
+      }}
+       onMouseLeave={()=>{
+        setNum2(num2+10)
+
+       }}
+
+
+      >Click</button>
     </div>
   )
 }
 
 export default App
+
