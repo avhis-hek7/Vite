da1e4bdd48aedf49828d46ff9503ee46d80772a7
avhis-hek7 <sharmaavishek679@gmail.com>
2025-12-10 17:31:37 +0545

    gallery Project is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,66 @@
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios'
 
 const App = () => {
-  // const [num, setNum] = useState(0)
-  // const [num2, setNum2] = useState(100)
-  // useEffect(function(){
-  //   console.log('use effect is running');
 
-  // },[num])
-  const [a, setA] = useState(0)
-  const [b, setB] = useState(0)
+  const [userData, setUserData] = useState([]);
 
-  function aChanging(){
-    console.log('A value changed');
-    
+  const [index, setIndex] = useState(1);
 
+  const getData = async () => {
+    // axios.post('https://picsum.photos/v2/list?page=2&limit=100',{username:'sarthak',password:'sarthakishere'})
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}`)
+    setUserData(response.data)
   }
 
-   function bChanging(){
-    console.log('B value changed');
-  }
+  useEffect(function(){
+    getData()
+  },[index])
 
-  useEffect (function(){
-   
-    console.log('use Effect is running');
-    
-  },[a])
+  let printUserData = <h3 className='text-gray-black text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Loading</h3>
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+      return <div key={idx}>
+        <a href={elem.url} target='_blnak'>
+
+        <div className='h-40 w-44  overflow-hidden bg-white rounded-xl'>
+          <img className='h-full w-full object-cover ' src={elem.download_url} alt="" />
+        </div>
+        <h2 className='font-bold text-1.5xl'>{elem.author}</h2>
+        </a>
+      </div>
+
+    })
+  }
   return (
-    <div>
+    <div className='bg-black h-screen text-white'>
+      <h1  className='fixed text-6xl'>{index}</h1>
+     
+      <div className='flex flex-wrap gap-4'>
+        {printUserData}
 
-      <h1>A is {a}</h1>
-      <h1>B is {b}</h1>
-      <button
-      onClick={()=>{
-        setA(a+1)
-  
-      }}
-      >Change A</button>
-      <button
-         onClick={()=>{
-        setB(b-1)
-  
-      }}
-      >Change B</button>
-      {/* <h1> Value of num1 is{num}</h1>
-      <h1> Value of num2 is{num2}</h1>
-      <button 
-      // onClick={()=>{
-      //   setNum(num+1)
-        
+      </div>
+      <div className='flex justify-center gap-6items-center p-4'>
+        <button className='bg-amber-400 text-black rounded px-4 py-2 font-semibold' onClick={()=>{
+          if(index>1){
+            setIndex(index-1)
+            setUserData([])
+          }
 
-      // }}
-      // onDoubleClick={()=>{
-      //   setNum2(num2+10)
-      // }}
-        
+        }}>Prev</button>
+        <h4>Page{index}</h4>
+        <button className='bg-amber-400 text-black rounded px-4 py-2 font-semibold' onClick={()=>{
+          setIndex(index+1)
+          setUserData([])
+          
 
-      onMouseEnter={()=>{
-        setNum(num+1)
-      }}
-       onMouseLeave={()=>{
-        setNum2(num2+10)
-
-       }}
-
-
-      >Click</button> */}
+          
+        }}>Next</button>
+      </div>
     </div>
   )
 }
 
 export default App
+
 
